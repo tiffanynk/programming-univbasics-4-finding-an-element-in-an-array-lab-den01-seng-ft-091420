0743f72aa613d08e2b29ea8dc1f10eac651b076c
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-08 18:55:44 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   index = 0
-  found_value_index = 0
+  found_value_index = nil
   
   while index < array.length do
     puts array[index] === value_to_find
