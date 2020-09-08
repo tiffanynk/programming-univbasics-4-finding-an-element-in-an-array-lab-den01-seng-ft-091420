ec9a2ea3cd5f71aef99390127280fe364ede6c87
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-08 19:02:14 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  index = 0
+
+  while index < array.length do
+    if array[index] == value_to_find
+# Access each element with array[index] and compare each aray element to value_to_find
+      return index
+#if a match is found between an array element and the value to find, we immediately
+#return index, breaking out of the loop.
+    end
+    index += 1
+  end
 end
+
+#This solution will always return the first index of a matching element in the provided array.
+#It also will stop working as soon as it finds a match, rather than continuing to loop.
