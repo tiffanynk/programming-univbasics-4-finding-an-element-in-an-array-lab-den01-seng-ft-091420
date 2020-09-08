5e4acffbc78fcd24a3faa49afd80c4039d8b9a3e
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-08 18:59:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,6 +3,7 @@ def find_element_index(array, value_to_find)
 
   while index < array.length do
     if array[index] == value_to_find
+# Access each element with array[index] and compare each aray element to value_to_find
       return index
     end
     index += 1
